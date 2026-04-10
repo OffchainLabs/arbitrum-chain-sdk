38e759fe72863ff72c77ef818f4daf9300a22e9a
Chris Buckland <cpbuckland88@gmail.com>
2026-04-10 13:42:13 +0100

    chore(scripting): rename testing.ts to schemaCoverage.ts, remove consumer test

--- src/scripting/schemaCoverage.unit.test.ts
+++ src/scripting/schemaCoverage.unit.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from 'vitest';
-import { mocks, assertSchemaCoverage } from './testing';
+import { mocks, assertSchemaCoverage } from './schemaCoverage';
 
 import { getValidatorsSchema, getValidatorsTransform } from './schemas/getValidators';
 import { getValidators } from '../getValidators';
