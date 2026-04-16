98fc130fc4289b08dcc2ed15a9054c24216495f8
Chris Buckland <cpbuckland88@gmail.com>
2026-04-16 16:32:38 +0100

    refactor(scripting): update coverage tests and examples for baked-in transforms
    schemaCoverage.unit.test.ts now uses schemas directly instead of
    schema.transform(resolver). Examples updated to match.

--- src/scripting/examples/getValidators.ts
+++ src/scripting/examples/getValidators.ts
@@ -1,8 +1,8 @@
 import { runScript } from '../scriptUtils';
-import { getValidatorsSchema, getValidatorsTransform } from '../schemas';
+import { getValidatorsSchema } from '../schemas';
 import { getValidators } from '../../getValidators';
 
-export const schema = getValidatorsSchema.transform(getValidatorsTransform);
+export const schema = getValidatorsSchema;
 
 export const execute = async (args: Parameters<typeof getValidators>) => getValidators(...args);
 
