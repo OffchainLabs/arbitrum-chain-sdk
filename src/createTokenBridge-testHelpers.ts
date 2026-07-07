c5021f9f740a3ed399ae4577d0fdd257b5f8bfff
alxdca <alexandre.dacosta1@gmail.com>
2026-07-07 11:03:52 +0200

    test: build token bridge docker image

--- src/createTokenBridge-testHelpers.ts
+++ src/createTokenBridge-testHelpers.ts
@@ -1,11 +1,33 @@
 import { PublicClient, Address } from 'viem';
-import { exec } from 'node:child_process';
+import { execFile } from 'node:child_process';
 import { promisify } from 'util';
 
 import { getNitroTestnodePrivateKeyAccounts } from './testHelpers';
 
-const execPromise = promisify(exec);
+const execFilePromise = promisify(execFile);
 const testnodeAccounts = getNitroTestnodePrivateKeyAccounts();
+const tokenBridgeContractsImage = 'arbitrum-chain-sdk-token-bridge-contracts:v1.2.2';
+
+let tokenBridgeContractsImagePromise: Promise<string> | undefined;
+
+async function buildTokenBridgeContractsImage() {
+  if (typeof tokenBridgeContractsImagePromise !== 'undefined') {
+    return tokenBridgeContractsImagePromise;
+  }
+
+  tokenBridgeContractsImagePromise = (async () => {
+    await execFilePromise(
+      'docker',
+      ['build', '-q', '-t', tokenBridgeContractsImage, 'token-bridge-contracts'],
+      {
+        maxBuffer: 1024 * 1024 * 10,
+      },
+    );
+    return tokenBridgeContractsImage;
+  })();
+
+  return tokenBridgeContractsImagePromise;
+}
 
 export async function deployTokenBridgeCreator({
   publicClient,
@@ -14,14 +36,29 @@ export async function deployTokenBridgeCreator({
 }): Promise<Address> {
   // https://github.com/OffchainLabs/token-bridge-contracts/blob/main/scripts/local-deployment/deployCreatorAndCreateTokenBridge.ts#L109C19-L109C61
   const weth = '0x05EcEffc7CBA4e43a410340E849052AD43815aCA';
+  const image = await buildTokenBridgeContractsImage();
 
-  const { stdout } = await execPromise(`
-      docker run --rm --net=host \
-        -e BASECHAIN_RPC=${publicClient.transport.url} \
-        -e BASECHAIN_DEPLOYER_KEY=${testnodeAccounts.deployer.privateKey} \
-        -e BASECHAIN_WETH=${weth} \
-        -e GAS_LIMIT_FOR_L2_FACTORY_DEPLOYMENT=10000000 \
-        $(docker build -q token-bridge-contracts) deploy:token-bridge-creator`);
+  const { stdout } = await execFilePromise(
+    'docker',
+    [
+      'run',
+      '--rm',
+      '--net=host',
+      '-e',
+      `BASECHAIN_RPC=${publicClient.transport.url}`,
+      '-e',
+      `BASECHAIN_DEPLOYER_KEY=${testnodeAccounts.deployer.privateKey}`,
+      '-e',
+      `BASECHAIN_WETH=${weth}`,
+      '-e',
+      'GAS_LIMIT_FOR_L2_FACTORY_DEPLOYMENT=10000000',
+      image,
+      'deploy:token-bridge-creator',
+    ],
+    {
+      maxBuffer: 1024 * 1024 * 10,
+    },
+  );
 
   const match = stdout.match(/L1TokenBridgeCreator: (0x[0-9a-fA-F]{40})/);
 
