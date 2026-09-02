5a13317ce2717f7c86be858811707766e2e6358b
Alex <33397242+alxdca@users.noreply.github.com>
2026-09-02 14:29:59 +0200

    chore: restructure repo (#739)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 export default defineConfig(
   {
-    ignores: ['node_modules/**', 'src/dist/**', 'coverage/**'],
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
   },
   ...tseslint.configs.recommended,
 )
